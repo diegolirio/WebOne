145896184c929e26be2edb99852b3118d5a843f0
Diego Lirio <diegolirio.dl@gmail.com>
2014-01-03 18:10:33 -0200

    ...

---
+++ src/br/com/tdv/servlet/AdicionaContatoServlet.java
@@ -0,0 +1,66 @@
+package br.com.tdv.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/AdicionaContatoServlet")
+public class AdicionaContatoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AdicionaContatoServlet() {
+        super();
+    }
+    
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	String nome = request.getParameter("nome");
+    	String endereco = request.getParameter("endereco");
+    	String email = request.getParameter("email");
+    	String data = request.getParameter("dataNascimento");
+    	
+    	try {
+        	// Dao ----> Gravando no DB.    		
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+    	
+    	PrintWriter out = response.getWriter();
+    	out.println("<html>");
+    	out.println("<body>");
+    	out.println("<h4>Gravado com sucesso!</h4>");
+    	out.println("<p>Nome: "+nome+"</p>");
+    	out.println("<p>Endereço: "+endereco+"</p>");
+    	out.println("<p>Email: "+email+"</p>");
+    	out.println("<p>Data de Nascimento: "+data+"</p>");
+    	out.println("<a href='add_contato.html'><<< Voltar</a>");
+    	out.println("</body>");    	
+    	out.println("</html>");
+    	
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/br/com/tdv/servlet/One.java
@@ -0,0 +1,45 @@
+package br.com.tdv.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(description = "One Servlet", urlPatterns = { "/One" })
+public class One extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public One() {
+        super();
+    }
+    
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	//super.service(request, response);
+    	PrintWriter out = response.getWriter();
+    	out.println("<html>");
+    	out.println("<body>");
+    	out.println("<h1>Hello OneServlet");
+    	out.println("</body>");    	
+    	out.println("</html>");
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
